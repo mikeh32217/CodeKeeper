68a44f92033cdcc2e0197131a199ba70694a4111
Mike Hankey <mikeh32217@yahoo.com>
2021-05-05 15:32:43 -0400

    Getting ready to try AvalonEdit...save point

--- CodeKeeper/Utilities/DocumentUtils.cs
+++ CodeKeeper/Utilities/DocumentUtils.cs
@@ -14,36 +14,14 @@ namespace CodeKeeper.Utilities
     public class DocumentUtils
     {
         private static string ParseString { get; set; }
+        public static string LoadedText { get; set; }
 
         static DocumentUtils()
         {
             ParseString = ConfigMgr.Instance.settingProvider.GetSingleValue("RegexForParse", "value");
         }
 
-        public static bool InsertTextInFile(string path, int line, string txt)
-        {
-            if (!File.Exists(path))
-            {
-                MessageBox.Show("No such file: <" + path + ">");
-                return false;
-            }
-
-            var text = new StringBuilder();
-            int cnt = 0;
-
-            foreach (string s in File.ReadAllLines(path))
-            {
-                if (cnt++ == line)
-                    text.Append(txt);
-
-                text.AppendLine(s);
-            }
-
-            File.WriteAllText(path, text.ToString());
-
-            return true;
-        }
-
+        // TODO Not using this yet but may use it in the future!
         public static bool InsertSnippetInFile(string path, int line, string tag)
         {
             if (!File.Exists(path))
@@ -80,72 +58,6 @@ namespace CodeKeeper.Utilities
             return true;
         }
 
-        public static bool ParseFile(string path)
-        {
-            if (!File.Exists(path))
-            {
-                MessageBox.Show("No such file: <" + path + ">");
-                return false;
-            }
-
-            Regex rx = new Regex(ParseString);
-            MatchCollection matches = null;
-            var text = new StringBuilder();
-            string res = string.Empty;
-
-            Parallel.ForEach(
-                File.ReadLines(path), //returns IEumberable<string>: lazy-loading
-                new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
-                (line, state, index) =>
-                {
-                 matches = rx.Matches(line);
-                if (matches.Count > 0)
-                {
-                    res = Utilities.ParseUtils.ParseSnippet(line);
-                    text.Append(res + "\n");
-                }
-                else
-                    text.AppendLine(line);
-                }
-            );
-
-            //foreach (string s in File.ReadAllLines(path))
-            //{
-            //}
-
-            File.WriteAllText(path + ".tmp", text.ToString());
-
-            return true;
-        }
-
-        public static string PreviewFile(string path)
-        {
-            if (!File.Exists(path))
-            {
-                MessageBox.Show("No such file: <" + path + ">");
-                return string.Empty;
-            }
-
-            Regex rx = new Regex(ParseString);
-            MatchCollection matches = null;
-            var text = new StringBuilder();
-            string res = string.Empty;
-
-            foreach (string s in File.ReadAllLines(path))
-            {
-                matches = rx.Matches(s);
-                if (matches.Count > 0)
-                {
-                    res = Utilities.ParseUtils.ParseSnippet(s);
-                    text.Append(res + "\n");
-                }
-                else
-                    text.AppendLine(s);
-            }
-
-            return text.ToString();
-        }
-
         public static string LoadFile(string path)
         {
             if (!File.Exists(path))
@@ -154,22 +66,27 @@ namespace CodeKeeper.Utilities
                     "Not able to load file", 
                     MessageBoxButton.OK,
                     MessageBoxImage.Error);
-                return string.Empty;
+                return "File not found";
+                    ;
             }
 
-            MemoryStream ms = new MemoryStream();
-            
+            MemoryStream LoadedMemoryStream = new MemoryStream();
+
             Parallel.ForEach(
                 File.ReadLines(path), //returns IEumberable<string>: lazy-loading
                 new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
                 (line, state, index) =>
                 {
-                    ms.Write(ENC.UTF8.GetBytes(line), 0, line.Length);
-                    ms.Write(new byte[] { 0x0a }, 0, 1);
+                    LoadedMemoryStream.Write(ENC.UTF8.GetBytes(line), 0, line.Length);
+                    LoadedMemoryStream.Write(new byte[] { 0x0a }, 0, 1);
                 }
             );
 
-            return ENC.UTF8.GetString(ms.ToArray());
+            LoadedText = ENC.UTF8.GetString(LoadedMemoryStream.ToArray());
+
+            LoadedMemoryStream.Dispose();
+
+            return LoadedText;
         }
     }
 }
